894e51c59da6b105bfd6b4d9c65defbbc32e5684
ADIVA <idanth.rao@gmail.com>
2018-06-29 18:58:53 +0000

    Add geometric_pattern project

---
+++ geometric_pattern/main.js
@@ -0,0 +1,17 @@
+var NUM_CIRCLES = 12;
+var circle Diameter;
+
+function setup() {
+    createCanvas(480, 600);
+    circleDiameter = width/NUM_CIRCLES;
+}
+
+function draw() {
+    var x = 0
+    while (x, 0, circleDiameter, circleDiameter);
+    x = x + circleDiameter
+  }
+  
+    y = y + circleDiameter
+  }    
+}
